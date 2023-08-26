7f5b35f45ae3b16131cde8c68def66f153a42187
LuisZa <zangrid17@gmail.com>
2023-08-25 22:13:31 -0500

    first commit

---
+++ lab2/formateo2.py
@@ -0,0 +1,3 @@
+radio=10
+area=3.14*(radio*radio)
+print(f"radio={radio}\narea=3.14*radio**2\nel area de un circulo con radio {radio} es {area}")
---
+++ lab2/longitud.py
@@ -0,0 +1,5 @@
+stringe = "Python"
+valor = len(stringe)
+valor = float(valor)
+valor = str(valor)
+print(valor)
