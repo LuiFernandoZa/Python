21aab77006dbcc19adb6e08d33fdba42b018ecdb
LuiFernandoZa <142746138+LuiFernandoZa@users.noreply.github.com>
2023-08-29 17:10:22 -0500

    Delete lab1 directory

--- lab1/factorial.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Factorial de n: "))
-resp=1
-for i in range(1,n+1):
-    resp*=i
-    
-print(f'su factorial es {resp}')
--- lab1/homework.py
+++
@@ -1,6 +0,0 @@
-l=0
-for i in range (5):
-   n = int(input("inserte numero: "))
-   l+=n
-num=l / 5
-print(f'la mediana de sus numeros es {num}') 
